c5ac00d4925f4318ca83edaa7e1a3760f7c18467
ilya <ilya.lytvynov@egt-ua.com>
2018-03-26 20:23:11 +0300

    Added about page

--- config/base.js
+++ config/base.js
@@ -9,7 +9,8 @@ const baseConf = (_path) => {
     // and add path and name to entry array
     const VENDORS_NAME = 'vendors';
     const entry = {
-        index: ['babel-polyfill', './src/index/index.js']
+        index: ['babel-polyfill', './src/index/index.js'],
+        about: ['babel-polyfill', './src/about/about.js']
     };
 
     const plugins = Object.keys(entry).reduce((acc, name) => {
